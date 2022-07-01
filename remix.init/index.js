7e80771e75b8dd23e3248a54552b40c01dfe8848
Paul Jankowski <8bittitan@gmail.com>
2022-06-30 23:38:56 -0400

    Updates remix.init

--- remix.init/index.js
+++ remix.init/index.js
@@ -18,17 +18,6 @@ async function main({ rootDirectory }) {
   const EXAMPLE_ENV_PATH = path.join(rootDirectory, '.env.example');
   const ENV_PATH = path.join(rootDirectory, '.env');
   const PACKAGE_JSON_PATH = path.join(rootDirectory, 'package.json');
-  const EXAMPLE_CI_CONFIG_PATH = path.join(
-    rootDirectory,
-    'remix.init',
-    'ci.yml',
-  );
-  const CI_CONFIG_PATH = path.join(
-    rootDirectory,
-    '.github',
-    'workflows',
-    'ci.yml',
-  );
 
   const REPLACER = 'screamo-stack-template';
 
@@ -38,11 +27,10 @@ async function main({ rootDirectory }) {
     // get rid of anything that's not allowed in an app name
     .replace(/[^a-zA-Z0-9-_]/g, '-');
 
-  const [readme, env, packageJson, ciConfig] = await Promise.all([
+  const [readme, env, packageJson] = await Promise.all([
     fs.readFile(README_PATH, 'utf-8'),
     fs.readFile(EXAMPLE_ENV_PATH, 'utf-8'),
     fs.readFile(PACKAGE_JSON_PATH, 'utf-8'),
-    fs.readFile(EXAMPLE_CI_CONFIG_PATH, 'utf-8'),
   ]);
 
   const DATABASE_URL = `mysql://root:@localhost:3306/${APP_NAME}_dev`;
@@ -50,11 +38,6 @@ async function main({ rootDirectory }) {
   const newEnv = env
     .replace(/^SESSION_SECRET=.*$/m, `SESSION_SECRET="${getRandomString(16)}"`)
     .replace(/^DATABASE_URL=.*$/m, `DATABASE_URL="${DATABASE_URL}"`);
-
-  const newCiConfig = ciConfig.replace(
-    /^{ SESSION_SECRET }$/m,
-    `${getRandomString(16)}`,
-  );
 
   const newReadme = readme.replace(
     new RegExp(escapeRegExp(REPLACER), 'g'),
@@ -72,7 +55,10 @@ async function main({ rootDirectory }) {
     fs.writeFile(README_PATH, newReadme),
     fs.writeFile(ENV_PATH, newEnv),
     fs.writeFile(PACKAGE_JSON_PATH, newPackageJson),
-    fs.writeFile(CI_CONFIG_PATH, newCiConfig),
+    fs.copyFile(
+      path.join(rootDirectory, 'remix.init', 'ci.yml'),
+      path.join(rootDirectory, '.github', 'workflows', 'ci.yml'),
+    ),
     fs.copyFile(
       path.join(rootDirectory, 'remix.init', 'gitignore'),
       path.join(rootDirectory, '.gitignore'),
@@ -92,11 +78,11 @@ async function main({ rootDirectory }) {
     Deploy script finished!
 
     - You're now ready to rock and roll 🤘
-      npm run dev
       
     (remix.init directory can safely be removed)
     `.trim(),
   );
 }
 
-module.exports = main;
+// module.exports = main;
+main({ rootDirectory: path.join(process.cwd(), '..') });
