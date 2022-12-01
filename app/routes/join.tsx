ae282c289dc94f4f601d3a913b26cb79c54283a4
Paul Jankowski <8BitTitan@gmail.com>
2022-12-01 09:47:09 -0500

    Updates all package versions

--- app/routes/join.tsx
+++ app/routes/join.tsx
@@ -29,7 +29,7 @@ export const action: ActionFunction = async ({ request, context }) => {
     const res = await authenticator.authenticate('credentials', req, {
       context,
       throwOnError: true,
-      successRedirect: '/sites',
+      successRedirect: '/dashboard',
     });
 
     return res;
